d8136d728707824aa0176902ca1f644e15a813dd
Josh Meier <joshua.meier2@gmail.com>
2018-05-20 16:51:38 -0700

    Added free to print.  Updated README

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # ft_ls
 The first project in 42's Systems Branch.
 
-The goal of this project is to recode some of the ls command's functionality.  The main behaviors this version must replicate are:
+The goal of this project is to recode some of MacOS's ls's functionality.  The main behaviors this version must replicate are:
 
 -R - Recursively list subdirectories encountered.
 
--a - Include all entries whose names begin with a ".".
+-a - Include all entries whose names begin with a '.'.
 
 -l - List in long format.
 
 -r - Reverse the order of the sort to get reverse lexicographical order or the oldest entries first.
 
 -t - Sort by time modified (most recently modified first) before sorting the operands by lexicographical order.
+
+Extra functionality added in includes:
+
+-A - Include all entries whose names begin with a '.', excluding the "." and ".." directories.
 
 To make the project, run make.
 To use the project, run ./ft_ls with any of the above flags.
--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 16:39:00 by jmeier            #+#    #+#             */
-/*   Updated: 2018/05/20 13:20:48 by josh             ###   ########.fr       */
+/*   Updated: 2018/05/20 15:32:59 by josh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ char	*fill_perm(struct stat stats, char *filename)
 
 	ret = (char *)ft_memalloc(sizeof(char) * 11);
 	ret[0] = (S_ISDIR(stats.st_mode)) ? 'd' : '-';
-    ret[1] = (stats.st_mode & S_IRUSR) ? 'r' : '-';
-    ret[2] = (stats.st_mode & S_IWUSR) ? 'w' : '-';
-    ret[3] = (stats.st_mode & S_IXUSR) ? 'x' : '-';
-    ret[4] = (stats.st_mode & S_IRGRP) ? 'r' : '-';
-    ret[5] = (stats.st_mode & S_IWGRP) ? 'w' : '-';
-    ret[6] = (stats.st_mode & S_IXGRP) ? 'x' : '-';
-    ret[7] = (stats.st_mode & S_IROTH) ? 'r' : '-';
-    ret[8] = (stats.st_mode & S_IWOTH) ? 'w' : '-';
-    ret[9] = (stats.st_mode & S_IXOTH) ? 'x' : '-';
+	ret[1] = (stats.st_mode & S_IRUSR) ? 'r' : '-';
+	ret[2] = (stats.st_mode & S_IWUSR) ? 'w' : '-';
+	ret[3] = (stats.st_mode & S_IXUSR) ? 'x' : '-';
+	ret[4] = (stats.st_mode & S_IRGRP) ? 'r' : '-';
+	ret[5] = (stats.st_mode & S_IWGRP) ? 'w' : '-';
+	ret[6] = (stats.st_mode & S_IXGRP) ? 'x' : '-';
+	ret[7] = (stats.st_mode & S_IROTH) ? 'r' : '-';
+	ret[8] = (stats.st_mode & S_IWOTH) ? 'w' : '-';
+	ret[9] = (stats.st_mode & S_IXOTH) ? 'x' : '-';
 	xattr = listxattr(filename, NULL, 0, XATTR_NOFOLLOW);
 	ret[10] = xattr > 0 ? '@' : ' ';
 	return (ret);
@@ -72,6 +72,11 @@ void	print_info(t_node *node, t_f *f)
 	}
 	ft_printf("%6jd ", (intmax_t)node->info->size); //There's a flag to print out based on block size too.  Look into that.
 	ft_printf("%s ", node->info->datestring);
+	free(node->info->perm);
+	free(node->info->owner);
+	free(node->info->group);
+	free(node->info->datestring);
+	free(node->info);
 }
 
 void	print(t_node *tree, t_f *f)
@@ -84,21 +89,20 @@ void	print(t_node *tree, t_f *f)
 		while (tree->files[++i])
 		{
 			tree->files[i]->info = find_info(tree, i);
-			tree->lt += tree->files[i]->info->links;
+			tree->lt += tree->files[i]->info->size;
 		}
-		ft_printf("total %d\n", tree->lt); //Is this blocks?
+		ft_printf("total %d\n", (tree->lt / 512) + (tree->lt % 512 ? 1 : 0));
 		i = -1;
 	}
-	sort(tree, f); //Once all the info has been created, I can sort the stuff based on whatever is in the info struct.
+	sort(tree, f);
 	while (tree->files[++i] != NULL)
 	{
-		if (tree->files[i]->name[0] == '.' && !f->a_flag)
+		if (tree->files[i]->name[0] == '.' && (!f->a_flag || !f->ua_flag))
 			continue ;
 		if (f->ua_flag && (!ft_strcmp(tree->files[i]->name, ".") ||
 			!ft_strcmp(tree->files[i]->name, "..")))
 			continue ;
 		f->l_flag ? print_info(tree->files[i], f) : 0;
 		ft_printf("%s\n", tree->files[i]->name);
-		f->l_flag ? free_info(tree->files[i]->info): 0;
 	}
 }
